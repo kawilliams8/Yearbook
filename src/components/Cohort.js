71f98db4096b6b6e8e4a449e75714a601a0c3ccd
Katie Williams <kawilliams8@gmail.com>
2019-08-14 18:44:55 -0600

    Remove 'delete' button from staff cards

--- src/components/Cohort.js
+++ src/components/Cohort.js
@@ -5,13 +5,21 @@ import './Cohort.css';
 const Cohort = (props) => {
 
     let cohortCards = props.people.map(person => {
-      let photo;
+      let photo, position;
       if (props.position === 'staff') {
         photo = person.photo;
       } else {
         photo = `https://picsum.photos/id/${1}/200/300`;
       }
-      return <Person key={person.id} id={person.id} name={person.name} quote={person.quote} superlative={person.superlative} photo={photo} removeStudent={props.removeStudent}/>
+      return <Person 
+      key={person.id} 
+      id={person.id} 
+      name={person.name} 
+      quote={person.quote} 
+      superlative={person.superlative} 
+      photo={photo} 
+      removeStudent={props.removeStudent}
+      position={props.position}/>
     })
 
       return (
--- src/components/Person.js
+++ src/components/Person.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import './Person.css';
 
-const Person = ({name, photo, quote, superlative, id, removeStudent}) => {
-  return (
-    <div className="Person">
-      <h3 className="name">{name.toUpperCase()}</h3>
-      <img src={photo} className="photo" alt={name}></img>
-      <h4 className="quote">"{quote}"</h4>
-      <h5 className="superlative">{superlative}</h5>
-      <button className="deleteButton" onClick={() => removeStudent(id)}>Delete</button>
-    </div>
-  )
+const Person = ({position, name, photo, quote, superlative, id, removeStudent}) => {
+  if (position === 'staff') {
+    return (
+      <div className="Person">
+        <h3 className="name">{name.toUpperCase()}</h3>
+        <img src={photo} className="photo" alt={name}></img>
+        <h4 className="quote">"{quote}"</h4>
+        <h5 className="superlative">{superlative}</h5>
+      </div>
+    )
+  } else {
+    return (
+      <div className="Person">
+        <h3 className="name">{name.toUpperCase()}</h3>
+        <img src={photo} className="photo" alt={name}></img>
+        <h4 className="quote">"{quote}"</h4>
+        <h5 className="superlative">{superlative}</h5>
+        <button className="deleteButton" onClick={() => removeStudent(id)}>DELETE</button>
+      </div>
+    )
+  }
 }
 
 export default Person;
