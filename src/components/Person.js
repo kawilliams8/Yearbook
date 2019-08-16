bf370437582855a5165b951dbc291bad34df91b7
Katie Williams <kawilliams8@gmail.com>
2019-08-15 21:01:11 -0600

    Add suppressContentEditableWarning=true in Person

--- src/components/Person.js
+++ src/components/Person.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './Person.css';
 
 const Person = ({position, name, photo, quote, superlative, id, removeStudent}) => {
-  if (position === 'staff') {
-    return (
+  return position === 'staff' ? 
+    (
       <div className="Person">
         <h3 className="name">{name.toUpperCase()}</h3>
         <img src={photo} className="photo" alt={name}></img>
@@ -11,17 +11,16 @@ const Person = ({position, name, photo, quote, superlative, id, removeStudent}) 
         <h5 className="superlative">{superlative}</h5>
       </div>
     )
-  } else {
-    return (
+  :
+    (
       <div className="Person">
-        <h3 className="name" contentEditable={true} >{name.toUpperCase()}</h3>
+        <h3 className="name" contentEditable={true} suppressContentEditableWarning="true">{name.toUpperCase()}</h3>
         <img src={photo} className="photo" alt={name}></img>
-        <h4 className="quote" contentEditable={true} >"{quote}"</h4>
-        <h5 className="superlative" contentEditable={true} >{superlative}</h5>
+        <h4 className="quote" contentEditable={true} suppressContentEditableWarning="true">"{quote}"</h4>
+        <h5 className="superlative" contentEditable={true} suppressContentEditableWarning="true">{superlative}</h5>
         <button className="deleteButton" onClick={() => removeStudent(id)}>DELETE</button>
       </div>
     )
-  }
 }
 
 export default Person;
