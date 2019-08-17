55dad3ad2c7390f2edd9aec41741dec0bc43c2f2
Katie Williams <kawilliams8@gmail.com>
2019-08-17 16:48:49 -0600

    Add beforeEach to Person test

--- src/Cohort/Cohort.test.js
+++ src/Cohort/Cohort.test.js
@@ -3,19 +3,24 @@ import { shallow } from 'enzyme';
 import Cohort from './Cohort';
 
 describe('Cohort', () => {
-  it('should match the snapshot with all data passed in correctly', () => {
-    const students = [
-        { id: 21, name: 'Abigail', quote: 'Howdy.', superlative: 'Most Likely to', photo: 'https://placekitten.com/200/300' },
-        { id: 22, name: 'Brian', quote: 'Hello.', superlative: 'Most Likely to', photo: 'https://placekitten.com/200/300' },
-        { id: 23, name: 'Charlotte', quote: 'Hey.', superlative: 'Most Likely to', photo: 'https://placekitten.com/200/300' },
-      ]
 
-    const wrapper = shallow(<Cohort 
+  let mockRemoveStudent, students, wrapper;
+
+  beforeEach(() => {
+    mockRemoveStudent = jest.fn();
+    students = [
+      { id: 21, name: 'Abigail', quote: 'Howdy.', superlative: 'Most Likely to', photo: 'https://placekitten.com/200/300' },
+      { id: 22, name: 'Brian', quote: 'Hello.', superlative: 'Most Likely to', photo: 'https://placekitten.com/200/300' },
+      { id: 23, name: 'Charlotte', quote: 'Hey.', superlative: 'Most Likely to', photo: 'https://placekitten.com/200/300' },
+    ]
+    wrapper = shallow(<Cohort
       position="students"
       people={students}
-      removeStudent={jest.fn()}
-      />);
-
+      removeStudent={mockRemoveStudent}
+    />)
+  });
+  
+  it('should match the snapshot with all data passed in correctly', () => {
     expect(wrapper).toMatchSnapshot();
   });
 });
--- src/Person/Person.test.js
+++ src/Person/Person.test.js
@@ -3,53 +3,53 @@ import { shallow } from 'enzyme';
 import Person from './Person';
 
 describe('Person', () => {
-  it('should match the snapshot with all staff data passed in correctly', () => {
-    const person = { 
-      id: 1, 
-      name: 'Eric', 
-      quote: 'You miss all the shots you don\'t take.', 
-      superlative: 'Most Likely to Win the PGA', 
-      photo: './eric.jpg' 
+
+  let person1, person2, mockRemoveStudent, wrapper1, wrapper2;
+
+  beforeEach(() => {
+    person1 = {
+      id: 1,
+      name: 'Eric',
+      quote: 'You miss all the shots you don\'t take.',
+      superlative: 'Most Likely to Win the PGA',
+      photo: './eric.jpg'
     }
-
-    const photo = person.photo;
-    const position = "staff"
-
-    const wrapper = shallow(<Person
-      key={person.id}
-      id={person.id}
-      name={person.name}
-      quote={person.quote}
-      superlative={person.superlative}
-      photo={photo}
+    person2 = {
+      id: 218,
+      name: 'Rosa',
+      quote: 'Accept advice.',
+      superlative: 'Most Likely to Write JavaScript',
+      photo: 'https://placekitten.com/200/300',
+      position: 'students'
+    }
+    mockRemoveStudent = jest.fn();
+    wrapper1 = shallow(<Person
+      id = { person1.id }
+      name = { person1.name }
+      quote = { person1.quote }
+      superlative = { person1.superlative }
+      photo = { person1.photo }
+      removeStudent = { mockRemoveStudent }
+      position = { "staff" } 
+      />);
+    wrapper2 = shallow(<Person
+      key={person2.id}
+      id={person2.id}
+      name={person2.name}
+      quote={person2.quote}
+      superlative={person2.superlative}
+      photo={person2.photo}
       removeStudent={jest.fn()}
-      position={position} />);
+      position={ "students" } />);
+  })
 
-    expect(wrapper).toMatchSnapshot();
+  it('should match the snapshot with all staff data passed in correctly', () => {
+    expect(wrapper1).toMatchSnapshot();
   });
 
   it('should match the snapshot with all student data passed in correctly', () => {
-    const person = { 
-      id: 218, 
-      name: 'Rosa', 
-      quote: 'Accept advice.', 
-      superlative: 'Most Likely to Write JavaScript', 
-      photo: 'https://placekitten.com/200/300' 
-    }
 
-    person.photo = `https://picsum.photos/id/${person.id}/200/300`;
-    const position = "students";
-
-    const wrapper = shallow(<Person
-      key={person.id}
-      id={person.id}
-      name={person.name}
-      quote={person.quote}
-      superlative={person.superlative}
-      photo={person.photo}
-      removeStudent={jest.fn()}
-      position={position} />);
-
-    expect(wrapper).toMatchSnapshot();
+    person2.photo = `https://picsum.photos/id/${person2.id}/200/300`;
+    expect(wrapper2).toMatchSnapshot();
   });
 });
